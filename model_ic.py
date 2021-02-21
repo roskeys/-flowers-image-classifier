70d1b08b37650aae3348f82b809ed47586cb2187
roskey <lsjsg@outlook.com>
2021-02-21 11:43:35 +0800

    update more functions

--- model_ic.py
+++ model_ic.py
@@ -8,6 +8,7 @@ from torch import optim
 import torch.nn.functional as F
 from torchvision import models
 
+
 # Define classifier class
 class NN_Classifier(nn.Module):
     def __init__(self, input_size, output_size, hidden_layers, drop_p=0.5):
@@ -23,67 +24,76 @@ class NN_Classifier(nn.Module):
         super().__init__()
         # Add the first layer, input to a hidden layer
         self.hidden_layers = nn.ModuleList([nn.Linear(input_size, hidden_layers[0])])
-        
+
         # Add a variable number of more hidden layers
         layer_sizes = zip(hidden_layers[:-1], hidden_layers[1:])
         self.hidden_layers.extend([nn.Linear(h1, h2) for h1, h2 in layer_sizes])
-        
+
         self.output = nn.Linear(hidden_layers[-1], output_size)
-        
+
         self.dropout = nn.Dropout(p=drop_p)
-        
+
     def forward(self, x):
         ''' Forward pass through the network, returns the output logits '''
-        
+
         # Forward through each layer in `hidden_layers`, with ReLU activation and dropout
+        x = nn.Flatten()(x)
         for linear in self.hidden_layers:
             x = F.relu(linear(x))
             x = self.dropout(x)
-        
+
         x = self.output(x)
-        
+
         return F.log_softmax(x, dim=1)
 
-# Define validation function 
+
+# Define validation function
 def validation(model, testloader, criterion, device):
     test_loss = 0
     accuracy = 0
-    
+
     for images, labels in testloader:
         images, labels = images.to(device), labels.to(device)
-                
+
         output = model.forward(images)
         test_loss += criterion(output, labels).item()
-        
+
         ps = torch.exp(output)
         equality = (labels.data == ps.max(dim=1)[1])
         accuracy += equality.type(torch.FloatTensor).mean()
 
     return test_loss, accuracy
 
+
 # Define NN function
-def make_NN(n_hidden, n_epoch, labelsdict, lr, device, model_name, trainloader, validloader, train_data):
+def make_NN(n_hidden, n_epoch, labelsdict, lr, device, model_name, trainloader, validloader, train_data, from_scratch,
+            train_all_parameters):
     # Import pre-trained NN model 
-    model = getattr(models, model_name)(pretrained=True)
-    
-    # Freeze parameters that we don't need to re-train 
-    for param in model.parameters():
-        param.requires_grad = False
-        
+    model = getattr(models, model_name)(pretrained=not from_scratch)
+
+    # Freeze parameters that we don't need to re-train
+    if not train_all_parameters:
+        for param in model.parameters():
+            param.requires_grad = False
+
     # Make classifier
-    n_in = next(model.classifier.modules()).in_features
-    n_out = len(labelsdict) 
+    if model_name == "densenet169":
+        n_in = next(model.classifier.modules()).in_features
+    else:
+        n_in = next(model.fc.modules()).in_features
+        model = torch.nn.Sequential(*(list(model.children())[:-1]))
+    n_out = len(labelsdict)
     model.classifier = NN_Classifier(input_size=n_in, output_size=n_out, hidden_layers=n_hidden)
-    
+
     # Define criterion and optimizer
     criterion = nn.NLLLoss()
-    optimizer = optim.Adam(model.classifier.parameters(), lr = lr)
+    optimizer = optim.Adam(model.classifier.parameters(), lr=lr)
 
     model.to(device)
     start = time.time()
 
     epochs = n_epoch
-    steps = 0 
+    steps = 0
     running_loss = 0
     print_every = 40
     for e in range(epochs):
@@ -110,16 +120,16 @@ def make_NN(n_hidden, n_epoch, labelsdict, lr, device, model_name, trainloader, 
                 with torch.no_grad():
                     test_loss, accuracy = validation(model, validloader, criterion, device)
 
-                print("Epoch: {}/{} - ".format(e+1, epochs),
-                      "Training Loss: {:.3f} - ".format(running_loss/print_every),
-                      "Validation Loss: {:.3f} - ".format(test_loss/len(validloader)),
-                      "Validation Accuracy: {:.3f}".format(accuracy/len(validloader)))
+                print("Epoch: {}/{} - ".format(e + 1, epochs),
+                      "Training Loss: {:.3f} - ".format(running_loss / print_every),
+                      "Validation Loss: {:.3f} - ".format(test_loss / len(validloader)),
+                      "Validation Accuracy: {:.3f}".format(accuracy / len(validloader)))
 
                 running_loss = 0
 
                 # Make sure training is back on
                 model.train()
-    
+
     # Add model info 
     model.classifier.n_in = n_in
     model.classifier.n_hidden = n_hidden
@@ -129,10 +139,11 @@ def make_NN(n_hidden, n_epoch, labelsdict, lr, device, model_name, trainloader, 
     model.classifier.optimizer_state_dict = optimizer.state_dict
     model.classifier.model_name = model_name
     model.classifier.class_to_idx = train_data.class_to_idx
-    
+
     print('model:', model_name, '- hidden layers:', n_hidden, '- epochs:', n_epoch, '- lr:', lr)
-    print(f"Run time: {(time.time() - start)/60:.3f} min")
+    print(f"Run time: {(time.time() - start) / 60:.3f} min")
     return model
+
 
 # Define function to save checkpoint
 def save_checkpoint(model, path):
@@ -148,22 +159,23 @@ def save_checkpoint(model, path):
                   'class_to_idx': model.classifier.class_to_idx
                   }
     torch.save(checkpoint, path)
-    
+
+
 # Define function to load model
 def load_model(path):
     cp = torch.load(path)
-    
+
     # Import pre-trained NN model 
     model = getattr(models, cp['model_name'])(pretrained=True)
-    
+
     # Freeze parameters that we don't need to re-train 
     for param in model.parameters():
         param.requires_grad = False
-    
+
     # Make classifier
     model.classifier = NN_Classifier(input_size=cp['c_input'], output_size=cp['c_out'], \
                                      hidden_layers=cp['c_hidden'])
-    
+
     # Add model info 
     model.classifier.n_in = cp['c_input']
     model.classifier.n_hidden = cp['c_hidden']
@@ -174,21 +186,22 @@ def load_model(path):
     model.classifier.model_name = cp['model_name']
     model.classifier.class_to_idx = cp['class_to_idx']
     model.load_state_dict(cp['state_dict'])
-    
+
     return model
 
-def test_model(model, testloader, device='cuda'):  
+
+def test_model(model, testloader, device='cuda'):
     model.to(device)
     model.eval()
     accuracy = 0
-    
+
     for images, labels in testloader:
         images, labels = images.to(device), labels.to(device)
-                
+
         output = model.forward(images)
-        
+
         ps = torch.exp(output)
         equality = (labels.data == ps.max(dim=1)[1])
         accuracy += equality.type(torch.FloatTensor).mean()
-    
-    print('Testing Accuracy: {:.3f}'.format(accuracy/len(testloader)))
+
+    print('Testing Accuracy: {:.3f}'.format(accuracy / len(testloader)))
--- train.py
+++ train.py
@@ -11,6 +11,8 @@ parser.add_argument("--hidden_units", type=int, default=1024, help="set hidden u
 parser.add_argument("--epochs", type=int, default=1, help="set epochs")
 parser.add_argument("--gpu", action="store_const", const="cuda", default="cpu", help="use gpu")
 parser.add_argument("--save_dir", help="save model")
+parser.add_argument("--scratch", default=False, help="Start from scratch")
+parser.add_argument("--all", default=False, help="Train all parameters of the model")
 
 args = parser.parse_args()
 
@@ -19,7 +21,7 @@ cat_to_name = read_jason(args.category_names)
 trainloader, testloader, validloader, train_data = load_data(args.data_dir)
 
 model = make_NN(n_hidden=[args.hidden_units], n_epoch=args.epochs, labelsdict=cat_to_name, lr=args.learning_rate, device=args.gpu, \
-                model_name=args.arch, trainloader=trainloader, validloader=validloader, train_data=train_data)
+                model_name=args.arch, trainloader=trainloader, validloader=validloader, train_data=train_data, from_scratch=args.scratch, train_all_parameters=args.all)
 
 if args.save_dir:
     save_checkpoint(model, args.save_dir)
